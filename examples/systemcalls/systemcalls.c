7b8f66473dc9cd5c7b326d20516ae6f0a26ca721
Jean Berniolles <2398126+thebrave@users.noreply.github.com>
2026-06-22 14:15:27 +0000

    assignment

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,99 +1,180 @@
 #include "systemcalls.h"
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
-bool do_system(const char *cmd)
-{
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+ */
+bool do_system(const char *cmd) {
+  int rval = system(cmd);
+  return rval != -1;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables
+ * are command to execute. followed by arguments to pass to the command Since
+ * exec() does not perform path expansion, the command to execute needs to be an
+ * absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in
+ * execv()
+ * @return true if the command @param ... with arguments @param arguments were
+ * executed successfully using the execv() call, false if an error occurred,
+ * either in invocation of the fork, waitpid, or execv() command, or if a
+ * non-zero return value was returned by the command issued in @param arguments
+ * with the specified arguments.
+ */
 
-bool do_exec(int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+bool do_exec(int count, ...) {
+  va_list args;
+  va_start(args, count);
+  char *command[count + 1];
+  int i;
+  for (i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+  }
+  command[count] = NULL;
+  // this line is to avoid a compile warning before your implementation is
+  // complete and may be removed
+  command[count] = command[count];
+
+  /*
+   * TODO:
+   *   Execute a system command by calling fork, execv(),
+   *   and wait instead of system (see LSP page 161).
+   *   Use the command[0] as the full path to the command to execute
+   *   (first argument to execv), and use the remaining arguments
+   *   as second argument to the execv() command.
+   *
+   */
+
+  // printf(">> do_exec - ");
+  // for (int i = 0; i < count; ++i) {
+  //   printf("%s ", command[i]);
+  // }
+  // printf("\n");
+
+  fflush(stdout);
+  bool is_ok = false;
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    execv(command[0], command);
+    exit(255);
+  } else {
+    int status;
+
+    // Wait for the specific child process to terminate
+    waitpid(pid, &status, 0);
+
+    // Check how the process terminated
+    if (WIFEXITED(status)) {
+      // Retrieve and print the actual return code
+      int return_code = WEXITSTATUS(status);
+      is_ok = return_code == 0;
+      // printf("Child process exited with return code: %d\n", return_code);
+    } else if (WIFSIGNALED(status)) {
+      // printf("Child process was killed by a signal: %d\n", WTERMSIG(status));
+    } else {
+      // printf("Child process terminated abnormally\n");
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+  }
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+  // printf("<< do_exec - is_ok=%s\n", is_ok ? "true" : "false");
 
-    va_end(args);
-
-    return true;
+  va_end(args);
+  return is_ok;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
-bool do_exec_redirect(const char *outputfile, int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
+bool do_exec_redirect(const char *outputfile, int count, ...) {
+  va_list args;
+  va_start(args, count);
+  char *command[count + 1];
+  int i;
+  for (i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+  }
+  command[count] = NULL;
+  // this line is to avoid a compile warning before your implementation is
+  // complete and may be removed
+  command[count] = command[count];
+
+  /*
+   * TODO
+   *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624
+   * as a refernce, redirect standard out to a file specified by outputfile. The
+   * rest of the behaviour is same as do_exec()
+   *
+   */
+
+  // printf(">> do_exec_redirect - ");
+  // for (int i = 0; i < count; ++i) {
+  //   printf("%s ", command[i]);
+  // }
+  // printf("\n");
+
+  int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+  if (fd < 0) {
+    perror("open");
+    return false;
+  }
+
+  fflush(stdout);
+  bool is_ok = false;
+  pid_t pid = fork();
+
+  switch (pid) {
+  case -1:
+    perror("fork");
+    abort();
+  case 0:
+    if (dup2(fd, 1) < 0) {
+      perror("dup2");
+      abort();
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+    close(fd);
 
+    execv(command[0], command);
+    exit(255);
+  default:
+    close(fd);
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    int status;
 
-    va_end(args);
+    // Wait for the specific child process to terminate
+    waitpid(pid, &status, 0);
 
-    return true;
+    // Check how the process terminated
+    if (WIFEXITED(status)) {
+      // Retrieve and print the actual return code
+      int return_code = WEXITSTATUS(status);
+      is_ok = return_code == 0;
+      // printf("Child process exited with return code: %d\n", return_code);
+    } else if (WIFSIGNALED(status)) {
+      // printf("Child process was killed by a signal: %d\n", WTERMSIG(status));
+    } else {
+      // printf("Child process terminated abnormally\n");
+    }
+  }
+
+  // printf("<< do_exec - is_ok=%s\n", is_ok ? "true" : "false");
+
+  va_end(args);
+
+  return is_ok;
 }
