a23f674ff04b9edda80df689a1111c979707e091
Jean Berniolles <2398126+thebrave@users.noreply.github.com>
2026-07-15 08:35:48 +0000

    assignment 5: now passes tests

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -30,6 +30,7 @@ void print_ipv4_info(struct sockaddr *addr, const char *prefix) {
   if (inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, INET_ADDRSTRLEN) != NULL) {
     // Convert port from network to host byte order
     uint16_t port = ntohs(ipv4->sin_port);
+    printf("%s %s:%d\n", prefix, ip_str, port);
     syslog(LOG_INFO, "%s %s:%d", prefix, ip_str, port);
   } else {
     perror("inet_ntop");
@@ -56,7 +57,7 @@ int main(int argc, char *argv[]) {
   int sockfd = socket(PF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     perror("socket");
-    exit(255);
+    return -1;
   }
 
   struct addrinfo hints;
@@ -69,38 +70,45 @@ int main(int argc, char *argv[]) {
 
   if (status != 0) {
     perror("getaddrinfo");
-    exit(255);
+    status = -1;
+    goto close_socket;
   }
   status = bind(sockfd, result->ai_addr, result->ai_addrlen);
   if (status == -1) {
     perror("bind");
-    exit(1);
+    freeaddrinfo(result);
+    goto close_socket;
   }
   freeaddrinfo(result);
 
   status = listen(sockfd, 0);
   if (status == -1) {
     perror("listen");
-    exit(255);
+    goto close_socket;
   }
 
   struct sockaddr cnx_addr;
   socklen_t cnx_addrlen = sizeof(struct sockaddr);
+  int output_file = -1;
 
   while (!should_die) {
     int cnx_fd = accept(sockfd, &cnx_addr, &cnx_addrlen);
     if (cnx_fd == -1) {
+      if (errno == EINTR) break; 
       perror("accept");
-      exit(255);
+      status = -1;
+      goto close_socket;
     }
     print_ipv4_info(&cnx_addr, "Accepted connection from");
 
     // Open the output file, creating this file if it doesn’t exist
-    int output_file =
-        open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND);
+    output_file = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND,
+             S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (output_file == -1) {
-      perror("fopen");
-      exit(255);
+      perror("open");
+      close(cnx_fd);
+      status = -1;
+      goto close_socket;
     }
 
     // Socket is open, loop until stream ends
@@ -111,13 +119,16 @@ int main(int argc, char *argv[]) {
 
     while (!should_die) {
       memset(tmp_buffer, 0, SMALL_BUF_SIZE);
+      printf("[d] Blocking read\n");
 
       // Blocking read from the socket
       ssize_t bytes_read = read(cnx_fd, tmp_buffer, SMALL_BUF_SIZE - 1);
       if (bytes_read < 1) {
+        printf("[d] Connection closed\n");
         break;
       }
-      buffer[strlen(tmp_buffer)] = '\0';
+
+      printf("[d] Received %zd bytes: \"%s\"\n", bytes_read, tmp_buffer);
 
       // Do I need to initialize a buffer ?
       if (buffer == NULL) {
@@ -131,64 +142,83 @@ int main(int argc, char *argv[]) {
         buffer[strlen(buffer)] = '\0';
       }
 
+      printf("[d] Current buffer %zd bytes: \"%s\"\n", strlen(buffer), buffer);
+
       // If there is a \n in the string,
       char *pos = strchr(buffer, '\n');
       if (pos != NULL) {
-        ssize_t len = pos - buffer;
+        printf("[d] Found a newline\n");
+        ssize_t len = pos - buffer + 1; // Include newline
         ssize_t bytes_written = write(output_file, buffer, len);
         if (bytes_written < 1) {
           perror("write");
-          exit(255);
-          break;
+          goto inner_cleanup;
         }
+        file_size += len;
 
         assert(bytes_written == len);
-
-        off_t file_size = lseek(output_file, 0, SEEK_CUR);
-        if (file_size == -1) {
-          perror("lseek_final");
-          exit(255);
-        }
+        printf("[d] Wrote %zd bytes to file\n", bytes_written);
+        break;
       }
     }
+    
+    if (buffer) {
+        free(buffer);
+        buffer = NULL;
+    }
 
+    printf("[d] Preparing to write to file\n");
     off_t res = lseek(output_file, 0, SEEK_SET);
     if (res == -1) {
       perror("lseek");
-      exit(255);
+      goto inner_cleanup;
     }
+    printf("[d] Position %zd bytes to file\n", res);
 
     // Copy Loop
     ssize_t bytes_read, bytes_written;
-    ssize_t total_written;
 
-    while ((bytes_read = read(output_file, buffer, file_size)) > 0) {
-      char *ptr = buffer;
-      total_written = 0;
+    printf("[d] Write loop\n");
+    while (true) {
+      memset(tmp_buffer, 0, SMALL_BUF_SIZE);
+
+      bytes_read = read(output_file, tmp_buffer, SMALL_BUF_SIZE);
+      printf("[d] Read %zd bytes\n", bytes_read);
+      if (bytes_read == 0) {
+        // EOF
+        break;
+      } else if (bytes_read < 0) {
+        perror("read");
+        goto inner_cleanup;
+      }
 
       // Loop to handle partial writes
+      ssize_t total_written = 0;
       while (total_written < bytes_read) {
-        bytes_written =
-            write(cnx_fd, ptr + total_written, bytes_read - total_written);
+        bytes_written = write(cnx_fd, tmp_buffer + total_written,
+                              bytes_read - total_written);
+        printf("[d] Wrote %zd/%zd bytes\n", bytes_written, total_written);
 
         if (bytes_written < 0) {
           // If interrupted by signal, retry; otherwise handle error
           if (errno == EINTR)
             continue;
-          close(cnx_fd);
-          close(output_file);
-          return -1;
+          goto inner_cleanup;
         }
         total_written += bytes_written;
       }
     }
 
+inner_cleanup:
+    free(tmp_buffer);
     // Close connexion
+    close(cnx_fd);
+    close(output_file);
+    printf("[d] Done writing\n");
     print_ipv4_info(&cnx_addr, "Closed connection from");
   }
 
-  // Stream ended, will cleanup
-  // TODO
-
-  return 0;
+close_socket:
+  close(sockfd);
+  return status;
 }
